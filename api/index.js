3f39d6b97e8bd2b8919228944fc04fe74a9104fd
Methembe Tshuma <53082789+CongoBeast@users.noreply.github.com>
2026-03-02 20:31:08 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -62,7 +62,7 @@ async function connectToDatabase() {
     await cachedClient.connect();
   }
 
-  cachedDb = cachedClient.db("nursing-school");
+  cachedDb = cachedClient.db("nursing-school-prod");
   return cachedDb;
 }
 
